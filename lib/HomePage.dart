acebda5f3ef3a7d786794ccb006ca465fd1baf59
avbigbuddy <136998037+avbigbuddy@users.noreply.github.com>
2023-07-25 11:01:31 +0530

    update

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -3,7 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 
 class Dashbord extends StatefulWidget {
   const Dashbord({super.key});
-
   @override
   State<Dashbord> createState() => _DashbordState();
 }
@@ -12,13 +11,88 @@ class _DashbordState extends State<Dashbord> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: false,
-        title: Text(
-          "Profile",
-          style: GoogleFonts.montserrat(color: Colors.black, fontSize: 20),
-        ),
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 80,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(color: Colors.white),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      width: 40,
+                    ),
+                    Text(
+                      "Profile",
+                      style: GoogleFonts.montserrat(
+                          color: Colors.black, fontSize: 20),
+                    )
+                  ],
+                )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(500),
+                        border: Border.all(width: 2, color: Colors.black),
+                      ),
+                      child: const Center(
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(500),
+                        border: Border.all(width: 2, color: Colors.black),
+                      ),
+                      child: const Center(
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 40, right: 40, top: 10),
+            child: Container(
+              height: 4,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
